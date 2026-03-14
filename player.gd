bea744179e5a5bbb45b6327ae8c180ffd62fc701
Roland Meyer <rmy@rmy.ch>
2026-03-14 15:23:52 +0100

    Basic player control

--- player.gd
+++ player.gd
@@ -1,35 +1,32 @@
 extends CharacterBody2D
 
+var angular_accelaration = 0.5
+var radius = 350
+var dampening_factor = 0.09
+
+
 var angle = 0
 var angular_speed = 0
-var max_angular_speed = 5
-var angular_accelaration = 1
-var angular_speed_decay = 1
-var radius = 350
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction = 0
 	if Input.is_action_pressed("ui_left"):
-		if angular_speed > -max_angular_speed:
-			angular_speed -= angular_accelaration
+		angular_speed -= angular_accelaration
 	if Input.is_action_pressed("ui_right"):
-		if angular_speed < max_angular_speed:
-			angular_speed += angular_accelaration
-	
-	# if angular_speed > 0:
-	# 	angular_speed -= angular_speed_decay
-	# elif angular_speed < 0:
-	# 	angular_speed += angular_speed_decay
+		angular_speed += angular_accelaration
+
+	if angular_speed > 0:
+		if angular_speed < dampening_factor:
+			angular_speed = 0
+		else:
+			angular_speed -= angular_speed * dampening_factor
+	elif angular_speed < 0:
+		if angular_speed > -dampening_factor:
+			angular_speed = 0
+		else:
+			angular_speed += -angular_speed * dampening_factor
 		
 	angle += angular_speed * delta
-		
+
 	if angle > 2*PI:
 		angle -= 2*PI
 	if angle < 0:
