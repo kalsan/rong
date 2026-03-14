98a6feb0fbd6d28b06ae0384bd103743a7bef714
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2026-03-14 13:54:27 +0100

    Spawn tiles

---
+++ Game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var tile_scene = preload("res://Tile.tscn")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(15):
+		var tile = tile_scene.instance()
+		add_child(tile)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
