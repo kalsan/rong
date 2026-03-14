74042b939b7591c9325862cffa589c554fd8338f
Njal Kuhn <git@njal.ch>
2026-03-14 15:08:03 +0100

    Fix Lobby

--- Game.gd
+++ Game.gd
@@ -9,6 +9,9 @@ var tile_scene = preload("res://Tile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Multiplayer
+	Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
+	
 	for i in range(15):
 		var tile = tile_scene.instantiate()
 		tile.global_position = Vector2(0, 0)
@@ -17,4 +20,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+
+# Called only on the server.
+func start_game():
+	# All peers are ready to receive RPCs in this scene.
 	pass
